75fffb0b02dbf6a080d883faff77a408621a8109
2anGel1 <angeemmanuelassamoi@gmail.com>
2023-02-19 22:27:40 +0000

    ddd

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,84 @@
+import 'dart:ffi';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ContactProfilePage());
+  runApp(ContactProfileApp());
 }
 
-class ContactProfilePage extends StatelessWidget {
+class ContactProfileApp extends StatefulWidget {
+  const ContactProfileApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  State<ContactProfileApp> createState() => _ContactProfileAppState();
+}
+
+class _ContactProfileAppState extends State<ContactProfileApp> {
+  var ic = true;
+  @override
+  build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blueGrey,
-              leading: IconButton(
-                icon: Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                ),
-                onPressed: () {},
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          leading: IconButton(
+            icon: Icon(Icons.navigate_before),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                ic ? Icons.star_border_outlined : Icons.star_sharp,
+                color: Colors.black,
               ),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(
-                    Icons.star_border,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    print("Star preseed");
-                  },
-                )
-              ],
-            ),
-            body: ListView(
+              onPressed: () {
+                setState(() {
+                  ic = !ic;
+                });
+              },
+            )
+          ],
+        ),
+        body: ListView(
+          children: <Widget>[
+            Column(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: double.infinity,
-                      height: 250,
-                      child: Image.network(
-                          "https://github.com/ptyagicodecamp/educative_flutter/raw/profile_1/assets/profile.jpg?raw=true",
-                          fit: BoxFit.cover),
-                    )
-                  ],
-                )
+                Container(
+                  height: 200,
+                  color: Colors.grey,
+                  width: double.infinity,
+                  child: Center(child: Text("image")),
+                  // child: Image.network(
+                  //     "https://github.com/ptyagicodecamp/educative_flutter/raw/profile_1/assets/profile.jpg?raw=true",
+                  //     fit: BoxFit.cover),
+                ),
+                Container(
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5.0),
+                        child: Text(
+                          "Petra Hargson",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.grey,
+                  thickness: 0.3,
+                ),
               ],
-            )));
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
